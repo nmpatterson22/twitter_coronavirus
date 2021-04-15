e98b328c6b0f9ad39cee5b23ecfd7926fd4a1627
npatterson22 <npatterson22@cmc.edu>
2021-04-15 16:40:44 -0700

    First Commit

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -56,13 +57,20 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # convert text to lower case
                 text = tweet['text'].lower()
+                try:
+                    lower = tweet['place']['country_code'].lower()
+                except TypeError:
+                    lower = 'NA'
+                    pass
 
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][lower] += 1
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][lower] += 1
 
 # open the outputfile
 try:
@@ -72,7 +80,11 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
-print('saving',output_path_lang)
+print('saving', output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
 
+output_path_country = output_path_base+'.country'
+print('saving', output_path_country)
+with open(output_path_country, 'w') as f:
+    f.write(json.dumps(counter_country))
